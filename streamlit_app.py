151ada4cce629c83c756c5ac905e62b10c658db5
Randy Zwitch <rzwitch@gmail.com>
2021-11-17 14:14:05 -0500

    Use widget labels to display question

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,18 @@ BASE_URL = "https://d4f1-69-174-156-186.ngrok.io/question/"
 ############## utilities
 @st.experimental_memo
 def get_question(lesson, question_id):
+    """Get question from API, return question_info key for convenience
+    instead of it nested inside the lesson key
+    """
     url = BASE_URL + lesson + "?" + "question_id=" + str(question_id)
     r = requests.get(url).json()
     return r["question_info"]
+
+
+def display_question(resp):
+    """Format the question nicely, given the API response"""
+    # return "Question " + str(resp["question_id"]) + ": " + resp["question_text"]
+    return f"""Question {resp["question_id"]}: {resp["question_text"]}"""
 
 
 ############## streamlit app
@@ -27,16 +36,23 @@ f"""
 ---
 """
 
-
 ### Is this a loop, where r["question_type"] determines the widget?
-q1 = get_question(lesson, 1)
-"Question ", str(q1["question_id"]), ": ", q1["question_text"]  # make an f-string?
+q0 = get_question(lesson, 0)
+q0_ans = st.radio(
+    display_question(q0),
+    q0["question_answers"],
+    format_func=lambda x: q0["question_answers"][x],
+)
 
+q1 = get_question(lesson, 1)
 q1_ans = st.radio(
-    "",
+    display_question(q1),
     q1["question_answers"],
     format_func=lambda x: q1["question_answers"][x],
 )
 
-# q1
-# q1_ans
+q2 = get_question(lesson, 2)
+q2_ans = st.text_input(display_question(q2))
+
+q3 = get_question(lesson, 3)
+q3_ans = st.text_input(display_question(q3))
