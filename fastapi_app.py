2054d6abc1fbab058d4f6e8c0f585a5966b58ae0
Zachary Blackwood <zblackwo@gmail.com>
2021-11-17 14:17:08 -0500

    Refactor and add answer endpoint

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,5 +1,6 @@
 import json
 from random import randint
+from typing import Literal
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -19,6 +20,9 @@ class Question(BaseModel):
     question_info: QuestionDetails
 
 
+Correctness = Literal["Correct", "Incorrect"]
+
+
 def get_quiz_contents(quiz_name: str) -> dict:
     with open("quizzes.json") as f:
         quizzes = json.load(f)
@@ -33,20 +37,30 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/question/{quiz}", response_model=Question)
-async def get_question(quiz: str, question_id: int = None):
+def get_quiz(quiz_name: str) -> dict:
     try:
-        contents = get_quiz_contents(quiz)
+        contents = get_quiz_contents(quiz_name)
     except KeyError:
-        raise HTTPException(status_code=400, detail=f"Invalid quiz: {quiz}")
-    if question_id is None:
-        question_id = randint(0, len(contents["questions"]) - 1)
+        raise HTTPException(status_code=400, detail=f"Invalid quiz: {quiz_name}")
+    return contents
+
+
+def get_question_details(quiz: dict, question_id: int) -> dict:
     try:
-        question = contents["questions"][question_id]
+        question = quiz["questions"][question_id]
     except IndexError:
         raise HTTPException(
             status_code=400, detail=f"Invalid question id: {question_id}"
         )
+    return question
+
+
+@app.get("/question/{quiz}", response_model=Question)
+async def get_question(quiz: str, question_id: int = None) -> Question:
+    contents = get_quiz(quiz)
+    if question_id is None:
+        question_id = randint(0, len(contents["questions"]) - 1)
+    question = get_question_details(contents, question_id)
     question_info = {
         "question_text": question["question"],
         "question_id": question_id,
@@ -54,3 +68,22 @@ async def get_question(quiz: str, question_id: int = None):
         "question_type": question["type"],
     }
     return {"quiz_name": contents["name"], "question_info": question_info}
+
+
+@app.put("/answer/{quiz}/{question_id}/")
+async def post_answer(quiz: str, question_id: int, answer: str) -> Correctness:
+    contents = get_quiz(quiz)
+    question = get_question_details(contents, question_id)
+
+    answer = answer.lower().strip()
+
+    print(question)
+    print(answer)
+
+    if question["answer"].lower() == answer:
+        return "Correct"
+
+    elif answer in question.get("alternative_answers", []):
+        return "Correct"
+
+    return "Incorrect"
